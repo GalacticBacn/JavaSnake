40fd04a30274d8d583f507b63fba606efec9e638
Harvey <Jak3grason@gmail.com>
2022-07-05 11:27:21 -0400

    Added drawApple method
    Apple is drawn to random location. Game still unfinished.

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -4,39 +4,90 @@ import java.awt.event.*;
 import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
+    // Game window width and height
     static final int SCREEN_WIDTH = 600;
     static final int SCREEN_HEIGHT = 600;
+
     static final int UNIT_SIZE = 25;
     static final int GAME_UNITS = ((SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE);
 
+    // the higher the delay the slower the game
+    static final int DELAY = 75;
+
+    // the snake body parts array, holds coordinates of snake parts
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts = 6;
+
+    // initialized at 0 with no values given
+    int applesEaten;
+    int appleX;
+    int appleY;
+
+    // Strings use "" while char uses '', char is a singular character
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
+
+    // game panel constructor
     GamePanel(){
-
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
     }
+
+    // starts game, called in the constructor
     public void startGame(){
-
+        newApple();
+        running = true;
+        timer = new Timer(DELAY,this);
+        timer.start();
     }
-    public void paintComponent(Graphics g){
 
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
     }
     public void draw(Graphics g){
-
+        for(int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++){
+            g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,SCREEN_HEIGHT);
+            g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE);
+        }
+        g.setColor(Color.red);
+        g.fillOval(appleX, appleY, UNIT_SIZE,UNIT_SIZE);
     }
+
+    // creates a new apple on screen
+    public void newApple(){
+        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
+    }
+
     public void move(){
 
     }
+
     public void checkApple(){
 
     }
+
     public void checkCollisions(){
 
     }
+
     public void gameOver(Graphics g){
 
     }
+
     @Override
     public void actionPerformed(ActionEvent e){
         // todo auto generated method stub
     }
+
     public class MyKeyAdapter extends KeyAdapter{
         @Override
         public void keyPressed(KeyEvent e){
